9048247ff3c408c4a93fcfa07358b4ce110b3da4
yuedenu <1657348384@qq.com>
2024-08-18 18:40:09 +0800

    update

--- exercises/01_variable&add/main.cpp
+++ exercises/01_variable&add/main.cpp
@@ -3,7 +3,7 @@
 // READ: 运算符 <https://zh.cppreference.com/w/cpp/language/expressions#.E8.BF.90.E7.AE.97.E7.AC.A6>
 
 int main(int argc, char **argv) {
-    // TODO: 补全变量定义并打印加法运算
+    // TODO: 补全变量定义并打印加法运算1
     // x ?
     int x = argc;
     std::cout << x << " + " << x << " = " << x + x << std::endl;
